d9bec73302f391c11fa1091bc997c4c80e624f19
Aman Chouhan <ak6927887@gmail.com>
2025-09-08 00:20:23 +0530

    Create Basic class & Object.cpp

---
+++ Basic class & Object.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class student{
+    public:
+    string name;
+    int age, roll;
+    string grad;
+};
+
+int main(){
+  
+    student s1;
+    s1.name="Aman";
+    s1.age=23;
+    s1.roll=8;
+    s1.grad="A+";
+
+    student s2;
+    s2.name="Amit";
+    s2.age=25;
+    s2.roll=9;
+    s2.grad="A++";
+
+    cout<<s1.name<<" "<<s1.age<<" "<<s1.roll<<" "<<s1.grad<<endl;
+    cout<<s2.name<<" "<<s2.age<<" "<<s2.roll<<" "<<s2.grad<<endl;
+
+}
