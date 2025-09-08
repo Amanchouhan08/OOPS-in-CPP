e387c08d48fbb8d07619ed595b67579fc749f2ea
Aman Chouhan <ak6927887@gmail.com>
2025-09-08 18:36:34 +0530

    Create Private with Function.cpp

---
+++ Private with Function.cpp
@@ -0,0 +1,31 @@
+//insert value in private class using function
+
+#include<iostream>
+using namespace std;
+
+class Student{
+    private:
+    string name;
+    int age,roll_no;
+    string grade;
+    public:
+    void setData(string n, int a, int r, string g){
+          name=n;
+          age=a;
+          roll_no=r;
+          grade=g;
+    }
+    void getData(){
+        cout<<name<<endl;
+        cout<<age<<endl;
+        cout<<roll_no<<endl;
+        cout<<grade<<endl;
+    }
+};
+
+int main(){
+    Student s1;
+    s1.setData("Aman", 23, 8, "A++");
+    s1.getData();
+    return 0;
+}
