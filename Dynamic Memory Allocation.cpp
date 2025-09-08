26f3097b91e585db1137ff64020d31e41de37f07
Aman Chouhan <ak6927887@gmail.com>
2025-09-09 00:44:26 +0530

    Create Dynamic Memory Allocation.cpp

---
+++ Dynamic Memory Allocation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Student{
+ public:
+  string name;
+  int age,roll;
+  string grad;
+};
+int main(){
+    Student *S=new Student;    //new is keyword use to Dynamic memory Allocation
+    (*S).name="Aman";
+     S->age=23;         //(*S) and S->  both line are same
+     (*S).roll=8;
+     S->grad="A++";
+
+     cout<<(*S).name<<" "<<S->age<<" "<<(*S).roll<<" "<<S->grad;
+     return 0;
+}
