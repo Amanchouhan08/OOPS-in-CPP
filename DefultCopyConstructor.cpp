2ae342697638fb9fe84f45df331dc6a386a9bd29
Aman Chouhan <ak6927887@gmail.com>
2025-09-11 14:09:28 +0530

    Create DefultCopyConstructor.cpp

---
+++ DefultCopyConstructor.cpp
@@ -0,0 +1,31 @@
+//Defult copy construction
+#include<iostream>
+using namespace  std;
+class Bank{
+     string name;
+     int balance;
+     int code;
+    public:
+    Bank(){
+      name="Aman";
+      balance=5000;
+      code=123;
+    }
+    void Display(){
+        cout<<name<<" "<<balance<<" "<<code<<endl;
+    }
+
+};
+
+int main(){
+    Bank B1;
+    B1.Display();
+    cout<<" copy the B1 data in B2"<<endl;
+    Bank B2(B1);
+    B2.Display();
+    cout<<" copy using assignment operator"<<endl;
+    Bank B3;
+    B3=B2;
+    B3.Display();
+    return 0;
+}
