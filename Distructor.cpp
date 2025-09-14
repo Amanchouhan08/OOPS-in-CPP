c1dc928580631afcd95d8d26b3a5df7bab2e8b6a
Aman Chouhan <ak6927887@gmail.com>
2025-09-14 23:52:47 +0530

    Update Distructor.cpp

--- Distructor.cpp
+++ Distructor.cpp
@@ -1,1 +1,22 @@
 //It is alast function which is going to call before objet is destory.
+//Constructor and distructor is created by the compiler itself.
+//but once we created the Constructor and distructor after that compiler will con creater this.
+#include<iostream>
+using namespace std;
+class Customer{
+   string name;
+   int age;
+   public:
+//constructor
+   Customer(){
+    cout<<"Constructor is created"<<endl;
+   }
+   //Distructor
+   ~Customer(){
+    cout<<"Distructor is created";
+   }
+};
+int main(){
+    Customer C1;
+    return 0;
+}
