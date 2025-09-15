9a500be56fdcb4bc680a7e7fc50165e1e6e82996
Aman Chouhan <ak6927887@gmail.com>
2025-09-15 23:33:36 +0530

    Update Encapsulation.cpp

--- Encapsulation.cpp
+++ Encapsulation.cpp
@@ -1,1 +1,41 @@
 //wrapping up of data & information in a single unit, while controlling access to them.
+#include<iostream>
+using namespace std;
+class Customer{
+  string name;
+  int balance;
+  public:
+  Customer(string name,int balance){
+    this->name=name;
+    this->balance=balance;
+  }
+  void Deposit_amount(int amount){
+       if(amount>0){
+        balance+=amount;
+       }
+       else{
+        cout<<"invalid amount"<<endl;
+       }
+  }
+   void withdraw_amount(int amount){
+       if(amount<=balance&&amount>0){
+        balance-=amount;
+       }
+       else{
+        cout<<"invalid amount"<<endl;
+       }
+  }
+  void Display(){
+    cout<<name<<" "<<balance<<endl;
+  }
+
+};
+int main(){
+    Customer C1("Aman",5000);
+    Customer C2("satya",10000);
+    C1.Deposit_amount(500);
+    C2.withdraw_amount(100);
+    C1.Display();
+    C2.Display();
+    return 0;
+}
