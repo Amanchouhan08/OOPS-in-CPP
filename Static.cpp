39efc3de77060f92147a5b8ca2c66129e1753d59
Aman Chouhan <ak6927887@gmail.com>
2025-09-15 22:27:27 +0530

    Create Static.cpp

---
+++ Static.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Customer{
+  string name;
+  int account,balance;
+  static int total_customer;
+  public:
+  Customer(string name, int account, int balance ){
+    this->name;
+    this->account;
+    this->balance;
+    total_customer++;
+  }
+  void Display(){
+    cout<<total_customer;
+  }
+};
+int Customer::total_customer=0;
+int main(){
+    Customer C1("Aman",1,5000);
+    Customer C2("satya",2,10000);
+    Customer C3("Amit",3,55555);
+    C1.Display();
+    return 0;
+}
